f8ead2e71a6447e1e500eda6b9f7d854883d330f
Tina Wang <mwow@yopmail.com>
2020-11-02 00:21:17 +0100

    ⚡️ Use modal wrapper for Contact Card component

---
+++ src/components/ContactCardModal.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="fixed h-full w-full position flex justify-center items-center">
+    <ContactCard/>
+  </div>
+</template>
+
+<script>
+import ContactCard from "~/components/ContactCard.vue";
+export default {
+  components: {
+    ContactCard
+  },
+
+}
+</script>
+
+<style scoped>
+.position {
+  top: 0;
+  left: 0;
+}
+</style>
