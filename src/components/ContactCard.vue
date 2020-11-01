7d097206482ac3f54a038a4ffd6d3fe0939f64cd
Tina Wang <mwow@yopmail.com>
2020-11-02 00:20:00 +0100

    📱 Improve responsivity

--- src/components/ContactCard.vue
+++ src/components/ContactCard.vue
@@ -1,12 +1,14 @@
 <template>
-  <div class="dimensions p-12 flex items-center border border-black">
-    <div class="flex flex-col justify-between bb">
+  <div
+    class="dimensions bg-white py-12 px-16 xl:px-24 flex items-center border portrait:flex-col-reverse"
+  >
+    <div class="flex flex-col flex-grow justify-between h-90 lg:h-60">
       <span
-        class=" text-sm"
+        class="text-xs lg:text-sm"
         data-aos="fade-up"
         data-aos-duration="1600"
         data-aos-delay="0"
-        >WANT TO SEE MORE?</span
+        >IT IS JUST THE BEGINNING</span
       >
       <Spacer
         data-aos="fade-up"
@@ -14,35 +16,44 @@
         data-aos-delay="150"
       />
       <span
-        class=" text-3xl leading-none"
+        class="text-xl lg:text-3xl leading-none"
         data-aos="fade-up"
         data-aos-duration="1600"
         data-aos-delay="250"
-        >_HIRE <strong>TINA</strong></span
+        >_WANT TO SEE <strong>MORE</strong> ?</span
       >
       <span
-        class=" text-sm"
+        class="text-xs lg:text-sm"
         data-aos="fade-up"
         data-aos-duration="1600"
         data-aos-delay="400"
-        >Flexible, adaptable 18-in, 20-out audio interface with vast
+        >Full stack developer <br />
         connectivity, powerful features, top-tier sound quality and a focus on
+        <br />
         creative workflow.</span
       >
       <div data-aos="fade-up" data-aos-duration="1800" data-aos-delay="650">
         <button
-          class="py-2  w-48  text-sm hover:bg-brand-black hover:text-brand-white border border-brand-black button"
+          class="py-1 w-32 text-xs lg:py-2 lg:w-48 lg:text-sm hover:bg-brand-black hover:text-brand-white border border-brand-black button"
         >
-          EXPLORE
+          HIRE ME
         </button>
       </div>
     </div>
-    <div class="flex flex-col justify-between items-center h-full">
-      <g-image class="h-48 w-48 rounded-full" src="~/assets/images/tina.jpg" />
-      <strong class="text-3xl ">Tina Wang</strong>
-      <g-image class="aa mx-auto" src="~/assets/images/motto.png" />
-      <Spacer />
-      <span class="text-lg">github.com/tinawng</span>
+    <div class="flex flex-col justify-between items-center h-full lg:h-95">
+      <g-image
+        class="h-32 w-32 lg:w-48 lg:h-48 xl:h-64 xl:w-64 rounded-full"
+        src="~/assets/images/tina.jpg"
+      />
+      <div class="flex flex-col items-center">
+        <strong class="text-xl lg:text-3xl xl:text-5xl">Tina Wang</strong>
+        <g-image
+          class="w-24 lg:w-32 xl:w-48 mx-auto"
+          src="~/assets/images/motto.png"
+        />
+      </div>
+      <Spacer width="6rem" />
+      <span class="text-xs xl:text-lg">github.com/tinawng</span>
     </div>
   </div>
 </template>
@@ -58,16 +69,15 @@ export default {
 
 <style scoped>
 .dimensions {
-  width: 100%;
-  max-width: 1033px;
-  height: 80vh;
-  max-height: 463px;
+  width: 70%;
+  max-width: 1333px;
+  height: 60%;
+  max-height: 650px;
+}
+.text-5xl {
+  font-size: 2.4rem;
 }
 .aa {
   width: 50%;
-}
-
-.bb {
-  height: 80%;
 }
 </style>
--- src/components/Spacer.vue
+++ src/components/Spacer.vue
@@ -1,16 +1,27 @@
-<template functional>
-  <div class="fix-width border-style"></div>
+<template>
+  <div ref="element" class="fix-width border-style"></div>
 </template>
 
 <script>
-export default {};
+export default {
+  props: {
+    width: {
+      type: String,
+      default: "3rem"
+    },
+  },
+  mounted() {
+    const element = this.$refs["element"];
+    element.style.width = this.width
+  }
+};
 </script>
 
 <style scoped>
 .border-style {
   border-bottom: 2px solid black;
 }
-.fix-width {
+/* .fix-width {
   width: 3rem;
-}
+} */
 </style>
