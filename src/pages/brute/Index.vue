79e673565e478810d8a85faa704f0b0199641e49
Tina Wang <mwow@yopmail.com>
2020-11-02 00:18:34 +0100

    🚧 Create futur pages

---
+++ src/pages/brute/Index.vue
@@ -0,0 +1,16 @@
+<template>
+<!-- https://www.sonos.com/fr-fr/products/wireless-speakers -->
+  <div>
+    brute
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/pages/brute/Polybrute.vue
@@ -0,0 +1,11 @@
+<template>
+  <!-- https://www.sonos.com/fr-fr/shop/five.html -->
+  <div>poly</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
