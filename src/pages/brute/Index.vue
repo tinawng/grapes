e6e888f3b161d995074b19dc47da5d719d23aded
Tina Wang <mwow@yopmail.com>
2020-11-03 23:15:42 +0100

    ⚡️ Load static data through GraphQL queries

--- src/pages/brute/Index.vue
+++ src/pages/brute/Index.vue
@@ -1,16 +1,31 @@
 <template>
-<!-- https://www.sonos.com/fr-fr/products/wireless-speakers -->
-  <div>
-    brute
-  </div>
+  <!-- https://www.sonos.com/fr-fr/products/wireless-speakers -->
+  <Layout>
+    <span>brute</span>
+    <div v-for="edge in $page.products.edges" :key="edge.node.name">
+      <h2>{{ edge.node }}</h2>
+    </div>
+  </Layout>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
+<page-query>
+query {
+  products: allProducts {
+    edges {
+      node {
+        name
+        range
+        baseline
+        price
+      }
+    }
+  }
+}
+</page-query>
 
+<style>
 </style>
--- src/pages/brute/Polybrute.vue
+++ src/pages/brute/Polybrute.vue
@@ -1,11 +1,22 @@
 <template>
   <!-- https://www.sonos.com/fr-fr/shop/five.html -->
-  <div>poly</div>
+  <div>
+    <span> poly </span>
+    <section v-html="$page.post.content" />
+  </div>
 </template>
 
 <script>
 export default {};
 </script>
+
+<page-query>
+query {
+  post: post(id: "polybrute") {
+    content
+  }
+}
+</page-query>
 
 <style>
 </style>
