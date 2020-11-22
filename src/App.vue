0ce96ade71cf88ae7138e7764d6973a9e7e2c836
Tina Wang <tina@tina.cafe>
2020-11-22 23:30:05 +0100

    💫 Add page transition

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="main-wrapper">
+      <transition name="fade" appear>
+        <router-view />
+      </transition>
+  </div>
+</template>
+
+<style>
+.fade-enter-active {
+  transition: opacity 5.5s;
+}
+
+.fade-enter {
+  opacity: 0;
+}
+</style>
