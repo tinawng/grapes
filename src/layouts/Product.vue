7bb3f69635c631cfa680fc74dfb76a50ee92ae4e
Tina Wang <tina@tina.cafe>
2020-11-27 17:00:16 +0100

    ✨ Add Freak pages

--- src/layouts/Product.vue
+++ src/layouts/Product.vue
@@ -1,13 +1,12 @@
 <template>
-  
+  <div class="bg-brand-white flex justify-center nav-ui-padding">
+    <slot />
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
 </style>
